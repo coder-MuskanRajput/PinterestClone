1fa337e4a403df8e424bb75a38f5ad42104fe233
coder-MuskanRajput <muskanrajput1510@gmail.com>
2024-01-09 01:17:30 +0530

    ading pages

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const userModel = require("./users")
 const postModel = require("./posts")
 
 const passport = require("passport")
+const upload  = require("./multer")
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
 /* GET home page. */
@@ -15,25 +16,44 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/login', function (req, res, next) {
-  console.log(req.flash("error"))
-  res.render('login', { error :req.flash("error") });
+  // console.log(req.flash("error"))
+  res.render('login', { error : req.flash("error") });
 });
 
 router.get("/feed" , function(req,res,next){
   res.render("feed")
 })
-router.get('/profile', isLoggedIn, function (req, res, next) {
-  res.render("profile")
+
+
+router.post("/upload" , isLoggedIn, upload.single("file"),async function(req,res,next){
+  if(!req.file){
+   return res.status(404).send("no files were given")
+  }
+  const user = await userModel.findOne({username : req.session.passport.user});
+    const post = await postModel.create({
+    image : req.file.filename,
+    imageText : req.body.fileCaption,
+    user : user._id
+ })
+   user.posts.push(post._id);
+   await user.save();
+  res.redirect("/profile");
+})
+
+router.get('/profile', isLoggedIn,async function (req, res, next) {
+   const user =  await userModel.findOne({
+          username : req.session.passport.user
+   }).populate("posts")
+   console.log(user);
+  res.render("profile", {user})
 });
 
 router.post("/register", function (req, res, next) {
   const { username, email, fullName } = req.body;
   const userData = new userModel({ username, email, fullName });
-
-
   userModel.register(userData, req.body.password)
     .then(function () {
-      passport.authenticate("local"), (req, res, function () {
+      passport.authenticate("local")(req, res, function () {
         res.redirect("/profile");
       })
     })
