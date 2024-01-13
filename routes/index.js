8ed011446797006b57aa37e704f928912b4bdefa
coder-MuskanRajput <muskanrajput1510@gmail.com>
2024-01-13 22:48:28 +0530

    nav add

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const passport = require("passport")
 const upload  = require("./multer")
 const localStrategy = require("passport-local");
 passport.use(new localStrategy(userModel.authenticate()));
-/* GET home page. */
 
+/* GET home page. */
 
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -48,6 +48,15 @@ router.get('/profile', isLoggedIn,async function (req, res, next) {
   res.render("profile", {user})
 });
 
+router.get('/addPost', isLoggedIn,async function (req, res, next) {
+  const user =  await userModel.findOne({
+         username : req.session.passport.user
+  }).populate("posts")
+  console.log(user);
+ res.render("profile", {user})
+});
+
+
 router.post("/register", function (req, res, next) {
   const { username, email, fullName } = req.body;
   const userData = new userModel({ username, email, fullName });
@@ -67,13 +76,20 @@ router.post("/login", passport.authenticate("local", {
 
 })
 
+router.post('/fileupload', isLoggedIn, upload.single("image") , async function (req, res, next) {
+ const user = await  userModel.findOne({username : req.session.passport.user});
+ user.dp = req.file.filename;
+ await user.save();
+ res.redirect("/profile")
+});
+
+
 router.get("/logout", function (req, res, next) {
   req.logout(function (err) {
     if (err) { return next(err); }
     res.redirect('/');
   });
 })
-
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next();
